4822d8b052d3ea4bd6cafaf748c7f2a32441ffb8
David Surkov <surkovdavid@gmail.com>
2022-01-22 20:45:40 +0100

    09 lesson changes

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ function App() {
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn react
         </a>
-        <h1>DAVID HUI</h1>
       </header>
     </div>
   );
