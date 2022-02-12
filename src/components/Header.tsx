5b0aad6d2758ce4c1f90b20d2dfdc81fedd7c3fb
David Surkov <surkovdavid@gmail.com>
2022-02-12 15:43:26 +0200

    09 lesson changes for Victoria

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src={'https://cdn.logo.com/hotlink-ok/logo-social.png'} alt={'LOGO'} />
+    </header>
+  );
+};
+
+export default Header;
