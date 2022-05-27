f95a895bc22f78a159282cfe44ed3fdb97a2e8a1
David Surkov <surkovdavid@gmail.com>
2022-05-27 14:29:25 +0300

    94_lesson changes for Victoria

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { lazy, useEffect } from 'react';
 import './App.css';
 import { AppStyle, Wrapper } from './components/styles/AppStyle';
 import { Route, Routes } from 'react-router-dom';
 import { Feed } from './components/Feed/Feed';
 import { Music } from './components/Music/Music';
 import { Settings } from './components/Settings/Settings';
-import { DialogsContainer } from './components/Dialogs/DialogsContainer';
 import { SidebarContainer } from './components/Navbar/SidebarContainer';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
@@ -16,6 +15,9 @@ import { initialiseAuthDataTC } from './redux/authorise_reducer';
 import { Preloader } from './components/Common/Preloader';
 import { AppRootStateType } from './redux/redux-store';
 import styled from 'styled-components';
+const DialogsContainer = lazy(() => import('./components/Dialogs/DialogsContainer'));
+
+//Styles
 const StyledContainer = styled.div`
   display: flex;
   width: 100%;
@@ -43,15 +45,17 @@ function App() {
       <HeaderContainer />
       <SidebarContainer />
       <Wrapper>
-        <Routes>
-          <Route path="/dialogs/*" element={<DialogsContainer />} />
-          <Route path="/profile/:userId" element={<ProfileContainer />} />
-          <Route path="/feed/*" element={<Feed />} />
-          <Route path="/music/*" element={<Music />} />
-          <Route path="/settings/*" element={<Settings />} />
-          <Route path="/users/*" element={<UsersContainer />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/dialogs/*" element={<DialogsContainer />} />
+            <Route path="/profile/:userId" element={<ProfileContainer />} />
+            <Route path="/feed/*" element={<Feed />} />
+            <Route path="/music/*" element={<Music />} />
+            <Route path="/settings/*" element={<Settings />} />
+            <Route path="/users/*" element={<UsersContainer />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+        </React.Suspense>
       </Wrapper>
     </AppStyle>
   );
--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { IUser } from '../../redux/users_reducer';
 import styled from 'styled-components';
 import { User } from './User';
-import Pagination from '../Common/Pagination';
+import { Pagination } from '../Common/Pagination';
 
 interface IUsers {
   users: Array<IUser>;
@@ -15,25 +15,31 @@ interface IUsers {
   followUserTC: (userId: number) => void;
 }
 //Styles
-const UsersStyle = styled.div``;
+const UsersStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 export const Users = (props: IUsers) => {
   return (
     <UsersStyle>
       <Pagination
-        totalUsersCount={props.totalUsersCount}
+        totalItemsCount={props.totalUsersCount}
         pageSize={props.pageSize}
         currentPage={props.currentPage}
         onPageChanged={props.onPageChanged}
       />
-      {props.users.map((user) => (
-        <User
-          user={user}
-          followingProgress={props.followingProgress}
-          unfollowUserTC={props.unfollowUserTC}
-          followUserTC={props.followUserTC}
-        />
-      ))}
+      <div>
+        {props.users.map((user) => (
+          <User
+            key={user.id}
+            user={user}
+            followingProgress={props.followingProgress}
+            unfollowUserTC={props.unfollowUserTC}
+            followUserTC={props.followUserTC}
+          />
+        ))}
+      </div>
     </UsersStyle>
   );
 };
